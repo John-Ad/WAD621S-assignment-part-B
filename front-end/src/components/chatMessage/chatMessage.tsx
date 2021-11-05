3f659f60e583f86a548eb1eb4fb5f6ef2982440e
John-Ad <adriaanscorne@gmail.com>
2021-11-05 07:20:45 +0200

    added logout functionality and updated message colour

--- front-end/src/components/chatMessage/chatMessage.tsx
+++ front-end/src/components/chatMessage/chatMessage.tsx
@@ -125,7 +125,7 @@ class ChatMessage extends React.Component<IProps, IState> {
     //###############################
     render() {
         return (
-            <div id="chat-message" className="flex-column center" style={{ backgroundColor: (this.props.message.Username === this.props.username) ? "#E2E2E2" : "#F0F0F0" }}>
+            <div id="chat-message" className="flex-column center" style={{ backgroundColor: (this.props.message.Username === this.props.username) ? "#D2D2D2" : "#E2E2E2" }}>
                 <div id="message-header" className="flex-row">
 
                     {
@@ -136,8 +136,8 @@ class ChatMessage extends React.Component<IProps, IState> {
                         <p>(edited)</p>
                     }
 
-                    <p style={{ color: "#8888FF" }}>{this.props.message.Username}</p>
-                    <p style={{ color: "#8888FF" }}>{this.props.message.Date_Added.replace(/T|.000|Z/gi, " ")}</p>
+                    <p style={{ color: "#5555FF" }}>{this.props.message.Username}</p>
+                    <p style={{ color: "#5555FF" }}>{this.props.message.Date_Added.replace(/T|.000|Z/gi, " ")}</p>
 
                     {
                         //###############################
--- front-end/src/components/header/header.tsx
+++ front-end/src/components/header/header.tsx
@@ -13,16 +13,25 @@ import "./header.css";
 
 
 //###############################
+//      INTERFACE DEFINITIONS
+//###############################
+
+interface IProps {
+    logout(): void
+}
+
+
+//###############################
 //      CLASS DEFINITION
 //###############################
 
-class Header extends React.Component {
+class Header extends React.Component<IProps> {
     render() {
         return (
             <div id="header-container" className="flex-row">
                 <h1>NUST Chat Room</h1>
-                <div className="flex-row">
-                    <h3>Account</h3>
+                <div className="flex-row" onClick={this.props.logout}>
+                    <h3>Log Out</h3>
                 </div>
             </div>
         )
